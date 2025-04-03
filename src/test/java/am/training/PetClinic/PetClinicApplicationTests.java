ddeb29a74314fd4a5cdface3fc20d524ff148505
Andreas Müller <dontwantthisseen@invisible.com>
2025-04-03 09:10:35 +0200

    Initial Project Setup

---
+++ src/test/java/am/training/PetClinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package am.training.PetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
